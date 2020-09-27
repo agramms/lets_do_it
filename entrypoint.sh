41331f91a1d7522237d0b22fc91651a9f558815e
Alessandro Vieira Grammelsbacher <alessandro.grammelsbacher@gmail.com>
2020-09-27 00:50:28 -0300

    Adding rakes on init path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# https://stackoverflow.com/a/38732187/1935918
 set -e
+
+if [ -f /app/tmp/pids/server.pid ]; then
+  rm /app/tmp/pids/server.pid
+fi
+
+bundle exec rake db:create 2>/dev/null || echo "database already exists"
+bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
 
 exec bundle exec "$@"
